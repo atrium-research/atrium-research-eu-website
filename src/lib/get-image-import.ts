d1d723764d51996b4032aa46c65bb8c4777e4853
Stefan Probst <stefanprobst@posteo.de>
2026-06-16 08:09:47 +0200

    fix: accept uppercase image file extensions

--- src/lib/get-image-import.ts
+++ src/lib/get-image-import.ts
@@ -4,7 +4,7 @@ import { assert } from "@acdh-oeaw/lib";
 import type { ImageMetadata } from "astro";
 
 const images = import.meta.glob<{ default: ImageMetadata }>(
-	"/public/assets/**/*.@(avif|bmp|gif|jpeg|jpg|png|svg|webp)",
+	"/public/assets/**/*.@(avif|AVIF|bmp|BMP|gif|GIF|jpeg|JPEG|jpg|JPG|png|PNG|svg|SVG|webp|WEBP)",
 );
 
 export function getImageImport(path: string) {
